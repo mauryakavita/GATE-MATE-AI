5e4b165ed5cfa329a25ba0a82731ae5b95a5e9b0
Kavita Maurya <147632467+mauryakavita@users.noreply.github.com>
2026-06-20 00:29:56 +0530

    Fixed env keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAK92nr_UrKn9RpK7ofrYWRVunorc7VJ-Q",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,  
   authDomain: "gateprep-ai-79ba8.firebaseapp.com",
   projectId: "gateprep-ai-79ba8",
   storageBucket: "gateprep-ai-79ba8.firebasestorage.app",
   messagingSenderId: "124431771601",
-  appId: "1:124431771601:web:b4f515059d8ad0f26ee2f0",
-  measurementId: "G-Q965ZXVEJS"
+  appId: "1:124431771601:web:b4f515859d8ad0f26ee2f0",
+  measurementId: "G-0965ZXVEJS"
 };
+
+
+
+
+
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
