e029fd5ef66a2a68dce1cf0d8e3199f98925dd65
Kavita Maurya <147632467+mauryakavita@users.noreply.github.com>
2026-06-29 12:03:24 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -25,24 +25,3 @@ export const auth = getAuth(app);
 
 
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,  
-//   authDomain: "gateprep-ai-79ba8.firebaseapp.com",
-//   projectId: "gateprep-ai-79ba8",
-//   storageBucket: "gateprep-ai-79ba8.firebasestorage.app",
-//   messagingSenderId: "124431771601",
-//   appId: "1:124431771601:web:b4f515059d8ad0f26ee2f0",
-//   measurementId: "G-Q965ZXVEJS"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
