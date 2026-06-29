2db2fa6792924f87027f94498ab58b2a14a49dfd
Kavita Maurya <147632467+mauryakavita@users.noreply.github.com>
2026-06-29 12:02:32 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -34,7 +34,7 @@ export const auth = getAuth(app);
 // // Your web app's Firebase configuration
 // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // const firebaseConfig = {
-//   apiKey: "AIzaSyAK92nr_UrKn9RpK7ofrYWRVunorc7VJ-Q",
+//     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,  
 //   authDomain: "gateprep-ai-79ba8.firebaseapp.com",
 //   projectId: "gateprep-ai-79ba8",
 //   storageBucket: "gateprep-ai-79ba8.firebasestorage.app",
